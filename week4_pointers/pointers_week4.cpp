ea261c5f33fc7109eb832fe0fdb18ec77d0a89b8
vaidik07 <110062522+vaidik07@users.noreply.github.com>
2023-07-04 19:24:10 +0530

    Add files via upload

---
+++ week4_pointers/pointers_week4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//Q1)
+// int main()
+// {
+    
+//     int x ,y;
+//     cin>>x>>y;
+//     int *ptr2=&y;
+    
+//     int *ptr1=&x;
+
+
+//     cout<<*ptr1<<endl;
+//     cout<<*ptr2<<endl;
+//     cout<<(*ptr1)*(*ptr2)<<endl;
+
+// }
+
+//Q2) 1st option
+
+//Q3) 1st option
+
+//Q4) 3rd option
+
+//Q5) correct
